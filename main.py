a1316e38535325bee1ee95043f2320cafa52e7fa
Vishweswar53 <vishweswar53@gmail.com>
2022-10-29 22:11:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pymongo
 
 # Provide the mongodb localhost url to connect python to mongodb.
-client = pymongo.MongoClient("mongodb://localhost:27020/neurolabDB")
+client = pymongo.MongoClient("mongodb://localhost:27017/neurolabDB")
 
 # Database Name
 dataBase = client["neurolabDB"]
